1e62027384df5693dd067272c8cdf2147e498222
David Ansermino <david@chainsafe.io>
2020-10-05 09:35:37 -0400

    Fix docker deployment (#27)

--- scripts/ci_docker.sh
+++ scripts/ci_docker.sh
@@ -8,18 +8,15 @@ TAG=${TAG:-"${TIMESTAMP}-${GIT_SHORT_COMMIT}"}
 
 case $TARGET in
   "default")
-    echo "Pushing image with tags \"latest\" and \"$TAG\""
-    docker build $BUILD_ARGS -t ${IMAGE_NAME}:${TAG} .
-    docker tag "${IMAGE_NAME}:${TAG}" "${IMAGE_NAME}:latest"
+    echo "Pushing image with tag $TAG"
+    docker tag ${IMAGE_NAME}:latest ${IMAGE_NAME}:${TAG}
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-    docker push ${IMAGE_NAME}:latest
     docker push ${IMAGE_NAME}:${TAG}
     ;;
 
   "release")
     echo "Pushing image with tag $TAG"
-    docker build $BUILD_ARGS -t ${IMAGE_NAME}:${TAG} .
-    docker tag "${IMAGE_NAME}:${TAG}"
+    docker tag ${IMAGE_NAME}:latest ${IMAGE_NAME}:${TAG}
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
     docker push ${IMAGE_NAME}:${TAG}
     ;;
